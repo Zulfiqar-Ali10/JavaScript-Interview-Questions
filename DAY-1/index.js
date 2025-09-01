d61450c51d832b512a85b27fa63494786908d4d0
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-09-01 16:16:49 +0500

    interview-Questions

--- DAY-1/index.js
+++ DAY-1/index.js
@@ -1,5 +1,41 @@
 /// Day 01 ///////////////////////////////////
 
+// x = 30 
+// console.log(`Value of X is ${x}`);
+// var x = 10;
+// console.log(`Value of X is ${x}`);
+
+/// Day 01 ///////////////////////////////////
+
+
+/// Day 01 ///////////////////////////////////
+
+// console.log("Global Execution Context starts");
+
+// var globalVariable = "I am a global variable";
+
+// function globalFunction() {
+//     console.log("Inside global function");
+// }
+
+// console.log(globalVariable);
+// globalFunction();
+
+// console.log("Global Execution Context ends");
+
+
+/// Day 01 ///////////////////////////////////
+
+
+/// Day 01 ///////////////////////////////////
+
+console.log("Start of script 2");
+
+setTimeout(() => {
+    console.log("This is from the Task Queue (setTimeout)");
+}, 0) 
+
+console.log("End of script ");
 
 
 
