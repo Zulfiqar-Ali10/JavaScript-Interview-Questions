9a3d2e955780b1265a66c1130bd47667643f2113
Zulfiqar Ali <157399741+Zulfiqar-Ali10@users.noreply.github.com>
2025-09-06 16:19:27 +0500

    Interview_Questions

--- DAY-4/index.js
+++ DAY-4/index.js
@@ -1,6 +1,24 @@
 
 /// Day 04 //////////////////////////////
 
-// In js Everything is an object? 
+// In js Everything is an object?
+ 
+const p1 = {
+    xp1: "I am inside P1",
+ };
 
+
+const p2 = {
+    xp2: "I am inside P2",
+    __proto__: p1,
+ };
+
+ 
+const p3 = {
+    xp3: "I am inside P3",
+    __proto__: p2,
+ };
+
+ console.log(p1, p2, p3);
+ 
 /// Day 04 //////////////////////////////
